68ab199b7126638c8865f3c424fc9ac8fb22326b
Abhi8835 <108941776+Abhi8835@users.noreply.github.com>
2022-08-06 20:14:29 +0530

    Add files via upload

--- map with lambda.py
+++ map with lambda.py
@@ -1,8 +1,8 @@
 lst = [2,4,8,5,7,1,2,3,6,9,7]
 def cube(lst):
-    return lst**3
+    return lst*3
 data = list(map(cube,lst))
 print(data)
 
-data = list(map(lambda lst : lst**3,lst))
+data = list(map(lambda lst : lst*3,lst))
 print (data)
